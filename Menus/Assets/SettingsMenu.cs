cdc4cd972ac32b7a2d6a1559255b71dfdb40f86e
eloi honnet <eloi.honnet@epita.com>
2020-02-09 20:34:04 +0100

    settings 1.0

---
+++ Menus/Assets/SettingsMenu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public AudioMixer audiomixer2;
+    
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetMusicVolume(float music)
+    {
+        audiomixer2.SetFloat("music", music);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+    
+    
+}
